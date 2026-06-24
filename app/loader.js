cd411e024a1a32fb2fb4ab97f6fe8f2e3c403bfb
allAboutJS <victoronah1914@gmail.com>
2026-06-24 10:59:02 +0100

    chore: add logic for debugging issue with node-fetch in production

--- app/loader.js
+++ app/loader.js
@@ -2,12 +2,14 @@ import fetch from "node-fetch";
 import { cart } from "../src/lib/cart.js";
 
 export async function GET() {
+  const response = await fetch("https://fakestoreapi.com/products/");
+
   try {
-    const response = await fetch("https://fakestoreapi.com/products/");
     const data = await response.json();
     return { products: data, cartCount: cart.length };
   } catch (error) {
     console.error(error);
+    console.log(await response.text());
     return { products: [], cartCount: cart.length };
   }
 }
