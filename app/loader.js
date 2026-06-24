0c665a17535dc2513b9f1021ee8c618a64cbdf06
allAboutJS <victoronah1914@gmail.com>
2026-06-24 10:42:35 +0100

    chore: add logging to root loader

--- app/loader.js
+++ app/loader.js
@@ -2,7 +2,12 @@ import fetch from "node-fetch";
 import { cart } from "../src/lib/cart";
 
 export async function GET() {
-  const response = await fetch("https://fakestoreapi.com/products/");
-  const data = await response.json();
-  return { products: data, cartCount: cart.length };
+  try {
+    const response = await fetch("https://fakestoreapi.com/products/");
+    const data = await response.json();
+    return { products: data, cartCount: cart.length };
+  } catch (error) {
+    console.error(error);
+    return { products: [], cartCount: cart.length };
+  }
 }
